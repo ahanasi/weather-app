37e2d22b80315886dec49e32d18ea9f86ce9464a
XXX Ahana <ahanasi@seas.upenn.edu>
2021-09-07 20:24:44 -0700

    Add ability to switch units

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 import "./style.css";
-var moment = require('moment');
+var moment = require("moment");
+
+const unknownCityAlert = document.querySelector(".unknown-city");
+const cityName = document.querySelector(".forecast-city");
+const celciusBtn = document.querySelector(".celcius-btn");
+const fhtBtn = document.querySelector(".fht-btn");
+const locationForm = document.getElementById("locationForm");
+const closeAlert = document.querySelector(".alert-close-btn");
+const cityTemp = document.querySelector(".main-temp");
+const feelsLike = document.querySelector(".feels-like");
+const maxTemp = document.querySelector(".max-temp");
+const minTemp = document.querySelector(".min-temp");
 
 const getWeather = async (location) => {
+  unknownCityAlert.classList.add("hidden");
+
   try {
     const data = await getData(location);
+
     console.log(data);
     return {
       city: data.name,
-      current_datetime: moment().utcOffset(data.timezone / 60).format("ddd, h:mm A"),
+      current_datetime: moment()
+        .utcOffset(data.timezone / 60)
+        .format("ddd, h:mm A"),
       weather_desc: data.weather[0].description,
       weather_icon: data.weather[0].icon,
       temp: data.main.temp,
@@ -16,6 +32,7 @@ const getWeather = async (location) => {
       feels_like: data.main.feels_like,
     };
   } catch (error) {
+    unknownCityAlert.classList.remove("hidden");
     throw new Error(e);
   }
 };
@@ -50,30 +67,60 @@ const tempInFarenheit = (kelvin) => {
 };
 
 const roundOffTemp = (tempInt) => {
-  return Math.round(tempInt * 100.0) / 100.0;
+  return Math.round(tempInt);
 };
 
-const locationForm = document.getElementById("locationForm");
+const switchTemp = ([avgT, feelT, maxT, minT], unit) => {
+  if (unit == "fht") {
+    cityTemp.textContent = `${roundOffTemp(tempInFarenheit(avgT))}°F`;
+    feelsLike.textContent = `${roundOffTemp(tempInFarenheit(feelT))}°F`;
+    maxTemp.textContent = `${roundOffTemp(tempInFarenheit(maxT))}°F`;
+    minTemp.textContent = `${roundOffTemp(tempInFarenheit(minT))}°F`;
+  } else {
+    cityTemp.textContent = `${roundOffTemp(tempInCelcius(avgT))}°C`;
+    feelsLike.textContent = `${roundOffTemp(tempInCelcius(feelT))}°C`;
+    maxTemp.textContent = `${roundOffTemp(tempInCelcius(maxT))}°C`;
+    minTemp.textContent = `${roundOffTemp(tempInCelcius(minT))}°C`;
+  }
+};
 
-locationForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  let locationInput = document.getElementById("location").value;
+const weatherInDOM = (unit, location = null) => {
+  let locationInput = location ? location : document.getElementById("location").value;
+
   getWeather(locationInput).then(
     (r) => {
-      console.log(r);
-      const cityName = document.querySelector(".forecast-city");
-      const cityTemp = document.querySelector(".main-temp");
       const weatherImg = document.querySelector(".weather-icon");
-      const weatherDesc = document.querySelector(".icon-desc");
+      const byline = document.querySelector(".forecast-byline");
 
-      cityName.innerHTML = r.city;
-      cityTemp.innerHTML = `${roundOffTemp(tempInCelcius(r.temp))}ºC`;
-      weatherImg.src = "http://openweathermap.org/img/wn/" + r.weather_icon + "@2x.png";
-      weatherImg.alt = "Weather Icon";
-      weatherDesc.innerHTML = r.weather_desc;
+      cityName.textContent = r.city;
+      weatherImg.src = "http://openweathermap.org/img/wn/" + r.weather_icon + "@4x.png";
+      byline.textContent = `${r.current_datetime}, ${r.weather_desc}`;
+      switchTemp([r.temp, r.feels_like, r.temp_max, r.temp_min], unit);
     },
     (reason) => {
       console.log(reason);
     }
   );
+};
+
+locationForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let unit = celciusBtn.classList.contains("opacity-50") ? "fht" : "cel";
+  weatherInDOM(unit);
+});
+
+closeAlert.addEventListener("click", (e) => {
+  unknownCityAlert.classList.add("hidden");
+});
+
+celciusBtn.addEventListener("click", () => {
+  celciusBtn.classList.remove("opacity-50");
+  fhtBtn.classList.add("opacity-50");
+  weatherInDOM("cel", cityName.textContent);
+});
+
+fhtBtn.addEventListener("click", () => {
+  fhtBtn.classList.remove("opacity-50");
+  celciusBtn.classList.add("opacity-50");
+  weatherInDOM("fht", cityName.textContent);
 });
